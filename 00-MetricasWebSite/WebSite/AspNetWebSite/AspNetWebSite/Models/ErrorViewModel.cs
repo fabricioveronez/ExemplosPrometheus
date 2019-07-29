81ef7e739bdd4ee494295a09b1328178f1e81c24
fabricioveronez <fabricioveronez@hotmail.com>
2019-07-28 21:51:41 -0300

    Inicio

---
+++ 00-MetricasWebSite/WebSite/AspNetWebSite/AspNetWebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetWebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ 00-MetricasWebSite/WebSite/AspNetWebSite/AspNetWebSite/Startup.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Metrics;
+using App.Metrics.Health;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetWebSite
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            var metrics = new MetricsBuilder()
+                               .Configuration.Configure(
+                                   options =>
+                                   {
+                                       options.AddServerTag();
+                                       options.AddEnvTag();
+                                       options.AddAppTag();
+                                   })
+                               .OutputMetrics.AsPrometheusPlainText()
+                               .Build();
+
+            services.AddMetrics(metrics);
+
+            var health = AppMetricsHealth.CreateDefaultBuilder().Report.ToMetrics(metrics).BuildAndAddTo(services);
+
+            services.AddMetricsReportingHostedService();
+            services.AddHealthReportingHostedService();
+
+            services.AddHealth(health);
+
+            services.AddMetricsEndpoints();
+            services.AddHealthEndpoints();
+            services.AddMetricsTrackingMiddleware();
+            services.AddMvc().AddMetrics();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseHealthAllEndpoints();
+            app.UseMetricsAllEndpoints();
+            app.UseMetricsAllMiddleware();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
